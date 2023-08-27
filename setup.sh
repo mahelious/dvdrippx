c4d253fc65fc62883d5bda90d1291bc553559611
Matthew Hill <mahelious@gmail.com>
2023-08-26 18:12:17 -0600

    fix outstanding issues with the setup script

--- setup.sh
+++ setup.sh
@@ -3,13 +3,11 @@
 # assumed environment:
 # Linux Mint 21.1 (upstream Ubuntu 22.04 Jammy Jellyfish)
 
+APP_DIR=$(dirname -- "$(readlink -f -- "$0";)")
+
 sudo apt-get -qq update --fix-missing \
   && sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y \
   && sudo apt-get install -y at curl flatpak git mkvtoolnix php-cli unzip
-
-sudo add-apt-repository -y ppa:heyarje/makemkv-beta > /dev/null 2>&1 \
-  && sudo apt-get -qq update \
-  && sudo apt-get install -y makemkv-bin makemkv-oss
 
 # install pup to scrape the makemkv beta key
 wget -O pup.zip https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip \
@@ -17,19 +15,21 @@ wget -O pup.zip https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v
   && sudo mv pup /usr/local/bin/ \
   && rm pup.zip
 
+## make the ripper command executable
+$sudo chmod +x $APP_DIR/rippx
+
+# install makemkv from the heyarje/makemkv-beta 3rd-party repo
+if [[ $(find /etc/apt/ -name *.list | xargs cat | grep "heyarje/makemkv-beta") -ne 0 ]]; then
+  sudo add-apt-repository -y ppa:heyarje/makemkv-beta > /dev/null 2>&1
+fi
+sudo apt-get -qq update \
+  && sudo apt-get install -y makemkv-bin makemkv-oss
+
 # scrape the makemkv beta key
 wget -O forum-key.html https://forum.makemkv.com/forum/viewtopic.php?t=1053 \
   && MAKEMKV_REG=$(cat forum-key.html | pup '#page-body .postbody .content code text{}') \
   && echo "app_Key = \"$MAKEMKV_REG\"" > .MakeMKV/settings.conf \
   && rm forum-key.html
-
-# setup logging
-# points for style, but this can be done later
-#sudo mkdir -p /var/log/dvdrippx
-#sudo chmod 755 /var/log/dvdrippx
-#sudo touch /var/log/dvdrippx/dvdrip.log
-#sudo chmod 644 /var/log/dvdrippx/dvdrip.log
-#echo -e "/var/log/dvdrippx/*log {\n\tdaily\n\trotate 7\n\tnocompress\n\tcreate\n\tmissingok\n\tnotifempty\n}" | sudo tee -a /etc/logrotate.d/dvdrippx
 
 # install ccextractor as a supplement for makemkv
 sudo apt-get install -y cargo clang tesseract-ocr libtesseract-dev libcurl4-gnutls-dev libleptonica-dev \
@@ -43,40 +43,33 @@ sudo apt-get install -y cargo clang tesseract-ocr libtesseract-dev libcurl4-gnut
   && sed -i "s/app_ccextractor = /#app_ccextractor = /" ~/.MakeMKV/settings.conf \
   && echo 'app_ccextractor = "/usr/bin/mmccextr"' >> ~/.MakeMKV/settings.conf
 
-# Recommended to install Handbrake from Flatpak
+# recommended to install Handbrake from Flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo \
   && sudo flatpak install -y flathub fr.handbrake.ghb
 
-## configure udev to mount in the real space, instead of the default behavior of mounting in a private space like an asshole
-#sudo mkdir -p /etc/systemd/system/systemd-udevd.service.d
-#echo -e "[Service]\nMountFlags=shared" | sudo tee -a /etc/systemd/system/systemd-udevd.service.d/mount_flags.conf
-## note this works in udev v237; starting in v239 instead run
-##echo -e "[Service]\nPrivateMounts=no" | sudo tee -a /etc/systemd/system/systemd-udevd.service.d/private_mounts.conf
-#
-# Get machine address of drive
-#
-# sudo lshw -c disk
-#   *-cdrom
-  #       description: DVD-RAM writer
-  #       product: BDDVDRW UH12NS29
-  #       vendor: HL-DT-ST
-  #       physical id: 0.0.0
-  #       bus info: scsi@2:0.0.0
-  #       logical name: /dev/cdrom
-  #       logical name: /dev/sr0
-# then use udevadm to capture the ID_PATH
-# $ sudo udevadm info -q all -n /dev/cdrom | grep "ID_PATH="
-# E: ID_PATH=pci-0000:07:00.0-ata-2.0
-#
-## setup udev to oblige handling input/output mounting
-# $ export DRIVE_ID_PATH="pci-0000:07:00.0-ata-2.0"
-# $ echo 'SUBSYSTEM=="block", ENV{ID_PATH}==$DRIVE_ID_PATH, ACTION=="change", RUN+="/opt/dvdrippx/drive_change.sh"' | sudo tee -a /etc/udev/rules.d/80-autodvd.rules
-#
-##$ sudo crontab -l
-##*/30 * * * * php /opt/dvdrippx/scripts/encode.php >> /opt/dvdrippx/logs/dvdrip.log 2>&1
-#
-##$ sudo cat /etc/udev/rules.d/autodvd.rules
-##SUBSYSTEM=="block", ENV{ID_PATH}=="pci-0000:00:17.0-ata-3", ACTION=="change", RUN+="/opt/dvdrippx/drive_change.sh"
-#
-## make the ripper command executable
-#$sudo chmod +x /opt/dvdrippx/rippx
+# configure udev to mount in the real space, instead of the default behavior of mounting in a private space like an asshole
+UDEV_CONF_DIR="/etc/systemd/system/systemd-udevd.service.d"
+if [ ! -d $UDEV_CONF_DIR ]; then
+  sudo mkdir -p $UDEV_CONF_DIR
+fi
+echo -e "[Service]\nPrivateMounts=no" | sudo tee $UDEV_CONF_DIR/private_mounts.conf
+
+# setup udev to automate handling input/output mounting
+# 1. Get machine address of drive
+ARG_IDPATH=udevadm info -q all -n /dev/cdrom | grep "ID_PATH="
+IFS='='
+read -ra ARR_IDPATH <<< "$ARG_IDPATH"
+# 2. Create the udev rule
+UDEV_RULE="SUBSYSTEM==\"block\", ENV{ID_PATH}==\"${ARR_IDPATH[1]}\", ACTION==\"change\", RUN+=\"$APP_DIR/drive_change.sh\""
+echo $UDEV_RULE | sudo tee /etc/udev/rules.d/80-autodvd.rules
+
+# add a cron entry to run the encoder every hour
+CRON_ENCODER_JOB="*/30 * * * * php $APP_DIR/scripts/encode.php"
+(crontab -l 2>/dev/null; echo $CRON_ENCODER_JOB) | crontab -
+
+# setup logging
+mkdir -p $APP_DIR/logs
+touch $APP_DIR/logs/dvdrip.log
+chmod 644 $APP_DIR/logs/dvdrip.log
+LOGROTATE="$APP_DIR/logs/* {\n\tdaily\n\trotate 7\n\tnocompress\n\tcreate\n\tmissingok\n\tnotifempty\n}"
+echo -e $LOGROTATE | sudo tee /etc/logrotate.d/dvdrippx
